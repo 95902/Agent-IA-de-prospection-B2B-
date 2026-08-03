f53ccf3d749eeb0b96f214257ff95ab458b1f1a0
Fabien Taupin <taupinfabien2607@gmail.com>
2026-08-04 01:01:46 +0200

    header des prospects

--- src/components/HeaderPropspect.tsx
+++ src/components/HeaderPropspect.tsx
@@ -1,3 +1,26 @@
+import { Calendar, Download } from "lucide-react";
+import { Button } from "./ui/Button";
+import { CardDescription, CardTitle } from "./ui/Card";
+
 export const HeaderPropspect = () => {
-  return <div>fdfd</div>;
+  return (
+    <div className="flex items-center justify-between mb-4">
+      <div className="flex flex-col space-y-1">
+        <CardTitle>Aperçu de la direction</CardTitle>
+        <CardDescription>
+          Informations en temps réel sur le pipeline et la vitesse des
+          campagnes.
+        </CardDescription>
+      </div>
+      <div className="flex items-center  gap-2">
+        <Button variant="outline" className="rounded-lg">
+          {" "}
+          <Calendar /> Les 30 derniers jours
+        </Button>
+        <Button className="rounded-lg">
+          <Download /> Exporter le report
+        </Button>
+      </div>
+    </div>
+  );
 };
